584dad7da8ba676b1dc0f0ebecca6465735400e2
shvetsovdm <sorrows@mail.ru>
2015-08-09 20:15:19 +1000

    Add Cars import actions

--- app/admin/catalog_car.rb
+++ app/admin/catalog_car.rb
@@ -5,5 +5,18 @@ module Catalog
 
     actions :index
 
+    action_item :import, only: :index do
+      link_to I18n.t('activeadmin.import'), action: :import
+    end
+
+    collection_action :import, method: :get do
+      render template: 'admin/catalog/cars/import'
+    end
+
+    collection_action :import, method: :post do
+      flash[:notice] = 'ok'
+      redirect_to action: :index
+    end
+
   end
 end
