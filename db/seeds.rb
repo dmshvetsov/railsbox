79239fd288c3b7839e424e94491d531642139bf8
shvetsovdm <sorrows@mail.ru>
2015-10-25 17:17:32 +1000

    Add pages creation in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,27 @@
 if Rails.env == 'development'
-  User.create email: 'shvetsovdm@gmail.com', password: '12345678'
+  # Users
+  User.create(email: 'shvetsovdm@gmail.com', password: '12345678')
+
+  # Pages
+  Structure::Page.delete_all
+
+  def create_page_stucture(type, attr, childs)
+    page = Structure::PageCreator.for(type.constantize, attr).create
+
+    childs.each do |subpage, subchilds|
+      subpage[:parent_id] = page.id
+      create_page_stucture(subpage[:type], subpage, subchilds)
+    end
+  end
+
+  {
+    { title: 'Car catalog', type: 'Structure::SectionPage', content: BasicSection.create(title: 'Car catalog') } => {
+      { title: 'SUV', type: 'Structure::SectionPage', content: BasicSection.create(title: 'SUV') } => {},
+      { title: 'Sedan', type: 'Structure::SectionPage', content: BasicSection.create(title: 'Sedan') } => {},
+      { title: 'Sports cars', type: 'Structure::SectionPage', content: BasicSection.create(title: 'Sports cars') } => {},
+      { title: 'Hatchback', type: 'Structure::SectionPage', content: BasicSection.create(title: 'Hatchback') } => {}
+    },
+    { title: 'About', type: 'Structure::ContentPage', content: BasicPage.create(title: 'About Us') } => {}
+  }.each { |page, childs| create_page_stucture(page[:type], page, childs) }
+
 end
