414543f058e28538b1943a35756be15b39729ede
shvetsovdm <sorrows@mail.ru>
2015-11-23 23:42:16 +1000

    Add raw solution to access children pages for cell
    Render children pages and their content in current section page

--- app/concepts/catalog/section/cell.rb
+++ app/concepts/catalog/section/cell.rb
@@ -1,10 +1,31 @@
 class Catalog::Section::Cell < Cell::Concept
 
+  include ActionView::Helpers::UrlHelper
+
+  property :title
+
   def show
     render
   end
 
+  def anons
+    render(:anons)
+  end
+
   private
+
+  def page
+    @options[:page]
+  end
+
+  def children
+    if page
+      ids = page.children.where(type: 'Structure::SectionPage').map(&:content_id)
+      collection = Catalog::Section.where(id: ids)
+      list = concept('catalog/section/cell', collection: collection, method: :anons)
+      "<ul>#{list}</ul>"
+    end
+  end
 
   def description
     model.description || 'No description'
--- app/controllers/structure/pages_controller.rb
+++ app/controllers/structure/pages_controller.rb
@@ -4,7 +4,7 @@ class Structure::PagesController < ApplicationController
     @page = Structure::Page.find_by_permalink(params[:permalink])
     content = @page.content
     content_cell_name = "#{content.class.name}::Cell"
-    @concept = concept(content_cell_name, content)
+    @concept = concept(content_cell_name, content, page: @page)
   end
 
 end
